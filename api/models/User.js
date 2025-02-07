5c9c57378582b7c10d6764ce425c08f5e27a5847
Noel Kim <wkim51@my.bcit.ca>
2025-02-07 04:06:26 -0800

    Initial commit

---
+++ api/models/User.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+import bcypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide a name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide an email'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+  },
+});
