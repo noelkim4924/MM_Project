74eeccd897079df0909ff885763b980458fd91cc
Davin Higo <dhigo1@my.bcit.ca>
2025-03-08 23:05:05 -0800

    removed extra logout button

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -21,16 +21,7 @@ const HomePage = () => {
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Homepage</h2>
         </div>
-        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-          <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10 border border-gray-200">
-            <button
-              onClick={handleLogout}
-              className="w-full py-2 px-4 border rounded-md bg-red-600 text-white shadow-sm"
-            >
-              Logout
-            </button>
-          </div>
-        </div>
+
       </div>
     </div>
   );
