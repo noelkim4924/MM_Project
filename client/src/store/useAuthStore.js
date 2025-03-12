7b53529dd1e923ec61955800608350cf6867d992
Noel Kim <wkim51@my.bcit.ca>
2025-03-11 23:30:53 -0700

    add socket.it

--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -1,6 +1,8 @@
 import { create } from 'zustand';
 import { axiosInstance } from '../lib/axios';
 import toast from 'react-hot-toast';
+import { disconnectSocket, initializeSocket } from '../socket/socket.client';
+
 
 export const useAuthStore = create((set) => ({
   authUser: null,
@@ -12,6 +14,7 @@ export const useAuthStore = create((set) => ({
       set({ loading: true });
       const res = await axiosInstance.post('/auth/signup', signupData);
       set({ authUser: res.data.user });
+      initializeSocket(res.data.user._id);
       toast.success('Signup successful');
     } catch (error) {
       toast.error(error.response.data.message || "Something went wrong");
@@ -25,6 +28,7 @@ export const useAuthStore = create((set) => ({
       set({ loading: true });
       const res = await axiosInstance.post('/auth/login', loginData);
       set({ authUser: res.data.user });
+      initializeSocket(res.data.user._id);
       toast.success('Logged in successful');
     } catch (error) {
       toast.error(error.response.data.message || "Something went wrong");
@@ -38,6 +42,7 @@ export const useAuthStore = create((set) => ({
     try {
       const res = await axiosInstance.post("/auth/logout");
       console.log("Logout response:", res);
+      disconnectSocket();
       if (res.status === 200) {
         set({ authUser: null });
         
@@ -51,6 +56,7 @@ export const useAuthStore = create((set) => ({
   checkAuth: async () => {
     try {
       const res = await axiosInstance.get('/auth/me');
+      initializeSocket(res.data.user._id);
       set({ authUser: res.data.user });
     } catch (error) {
       set({ authUser: null });
