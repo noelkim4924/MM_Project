739d564afb98180bc6960947c1b6496afaeb5be1
Davin Higo <dhigo1@my.bcit.ca>
2025-04-03 17:16:30 -0700

    changed admin check to role

--- client/src/components/admin/AdminEditUser.jsx
+++ client/src/components/admin/AdminEditUser.jsx
@@ -138,6 +138,20 @@ const AdminEditUser = () => {
                 </div>
               </div>
 
+              {/* <div>
+                <label className="block text-sm font-medium text-gray-700">
+                  Age
+                </label>
+                <div className="mt-1">
+                  <input
+                    type="text"
+                    value={age}
+                    onChange={(e) => setAge(e.target.value)}
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
+                  />
+                </div>
+              </div> */}
+
               <div>
                 <label className="block text-sm font-medium text-gray-700">
                   Gender
